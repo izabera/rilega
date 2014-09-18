04891008b9e16b553d3eafa588308f9e9cfb3d25
izabera <izaberina@gmail.com>
2014-09-18 14:21:28 -0400

    moved things around (wow this is such a good message for a commit)

--- fulldoc.php
+++ fulldoc.php
@@ -5,12 +5,36 @@ echo <<<END
 Table of contents
 ========
 
+- Version
 - Usage
 - File visibility
 - Directory structure
+- Limitations
+- Paying users' restrictions
+- Fair usage
 - Why this instead of RandomOtherService?
 - Bugs
 - Support us
+
+
+
+Version
+========
+
+This is rilega 0.1, first public release.
+
+We're running TeXLive 2014.
+
+The following compilers are supported:
+- pdftex
+- pdflatex
+- luatex
+- lualatex
+- xetex
+- xelatex
+
+For multiple compiles and special options, use arara.
+However, \write18 is disabled for security reasons.
 
 
 
@@ -60,12 +84,14 @@ Directory structure
 ========
 
 rile.ga/myfile                same as rile.ga/myfile/pdf/last
+rile.ga/myfile/v3             same as rile.ga/myfile/pdf/last/v3
 
 rile.ga/myfile/pdf            same as rile.ga/myfile/pdf/last
-rile.ga/myfile/pdf/last       pdf output of rile.ga/myfile/tex/last
-rile.ga/myfile/pdf/v3         pdf of third version
+rile.ga/myfile/pdf/last       pdf output of rile.ga/myfile/src/last
+rile.ga/myfile/pdf/v3         pdf output of rile.ga/myfile/src/v3
 
 rile.ga/myfile/png            same as rile.ga/myfile/png/last
+rile.ga/myfile/png/p5         same as rile.ga/myfile/png/last/p5
 rile.ga/myfile/png/last       png output of rile.ga/myfile/src/last , page 1
 rile.ga/myfile/png/last/p5    png output of rile.ga/myfile/src/last , page 5
 rile.ga/myfile/png/v3         png of third version, page 1
@@ -81,6 +107,10 @@ rile.ga/myfile/log/v3         compile log of third version
 
 rile.ga/user/myname           your profile page with all your documents
 
+rile.ga/license               full text of GNU Affero GPL v3
+
+rile.ga/doc                   full documentation (this page)
+
 
 
 
@@ -90,11 +120,11 @@ Limitations
 Time limitations
 --------
 
-- Anonymous users have up to 15s compile time.
-  If you require multiple compile runs, 15s will be the total time.
+- Anonymous users have up to 10s compile time.
+  If you require multiple compile runs, 10s will be the total time.
 
-- Registered users have up to 1m compile time.
-  If you require multiple compile runs, 1m will be the total time.
+- Registered users have up to 30s compile time.
+  If you require multiple compile runs, 30s will be the total time.
 
 These limitations don't apply to the png conversion time.
 
@@ -109,13 +139,39 @@ Input file limitations
 Output file limitations
 --------
 
+The following limitations only apply to the .pdf output size.
+
 - Anonymous users' output can take up to 2MiB.
 - Registered users' output can take up to 20MiB.
 
-These limitations only apply to the .pdf output size.
+If your output is bigger, a notice will be displayed instead.
 
 
-If you need more time/space, let us know.
+
+
+Paying users' restrictions
+========
+
+If you donate 10 USD or more, all these limitations are gone forever.
+Thus, if you need more time/space, please consider making a donation.
+However, fair usage limitations still apply (see below).
+
+
+
+
+Fair usage
+========
+
+We're not enforcing any limit on how many files you can upload.
+However, our server is not free: each compile costs us cpu power and space.
+
+Since this is primarly meant to be a file sharing service, we need to ask you
+not to use it as your only LaTeX compile resource.
+
+E.g.: sharing your research thesis with a ton of TikZ figures is totally fine.
+Uploading it every time you change a single character is not.
+
+We hope you'll understand.
 
 
 
@@ -124,6 +180,7 @@ Why this instead of RandomOtherService?
 ========
 
 - This is free and will be free forever
+- rilega is free software, released under the GNU Affero GPL license v3
 - The full source code is available on GitHub
 - Command line interface, easy to automate and script
 
--- index.php
+++ index.php
@@ -1,67 +1,38 @@
 <?php
-header("Content-Type: text/plain");
-
-echo <<<END
-What's rile.ga?
-========
-
-rile.ga is a file sharing service focused on TeX/LaTeX files.
-Send us your LaTeX files, we'll compile them and link you the output.
-
-Currently, we accept:
-- .tex files
-- .zip archives
-- .tar.gz archives.
-
-We'll compile the file master.tex if you upload an archive.
-You can specify multiple compile passes with arara.
-
-
-
-
-Basic usage
-========
-
-Anonymous upload
---------
-
-$ cat file.tex | curl -F 'rilega=<-' http://rile.ga
-http://rile.ga/abc123
-
-
-You can specify which compiler to use
---------
-
-$ cat file.tex | curl -F 'rilega=<-' -F 'c=xelatex' http://rile.ga
-http://rile.ga/efg456
-
-
-Registered users can edit their files
---------
-
-$ cat file.tex | curl -F 'rilega=<-' -F 'u=user' -F 'p=pass' http://rile.ga
-http://rile.ga/asdf56
-
-$ cat file.tex | curl -F 'rilega=<-' -F 'u=user' -F 'p=pass' -F 'f=asdf56' http://rile.ga
-http://rile.ga/asdf56 updated
-
-
-Compile your document as soon as you save it
---------
-
-Download http://rile.ga/pyrilega.zip , extract it and follow the instructions.
-It will monitor your folder, send us your .tex and download the generated pdf.
-
-
-
-
-
-
-
-
-Register at http://rile.ga/register
-
-Read the full documentation at http://rile.ga/doc .
-
-END;
+  if (isset($_POST['rilega'])) {
+    include "insert.php";
+  }
+  else if (isset($_GET['pdf'])) {       // rile.ga/myfile     == rile.ga/?pdf=myfile
+    include "pdf.php";
+  }
+  else if (isset($_GET['src'])) {       // rile.ga/myfile/src == rile.ga/?src=myfile
+    include "src.php";
+  }
+  else if (isset($_GET['png'])) {       // rile.ga/myfile/png == rile.ga/?png=myfile
+    include "png.php";
+  }
+  else if (isset($_GET['log'])) {       // rile.ga/myfile/log == rile.ga/?pdf=myfile
+    include "pdf.php";
+  }
+  else if (isset($_GET['register'])) {  // rile.ga/register   == rile.ga/?register
+    include "register.php";
+  }
+  else if (isset($_POST['c'])) {        // c=code
+    include "activateuser.php";
+  }
+  else if (isset($_POST['u']) && isset($_POST['p']) && isset($_POST['m'])) {
+    include "createuser.php";
+  }
+  else if (isset($_GET['doc'])) {       // rile.ga/doc        == rile.ga/?doc
+    include "fulldoc.php";
+  }
+  else if (isset($_GET['user'])) {      // rile.ga/user       == rile.ga/?user
+    include "userpage.php";
+  }
+  else if (isset($_GET['license'])) {   // rile.ga/license    == rile.ga/?license
+    include "license.php";
+  }
+  else {
+    include "shortdoc.php";
+  }
 ?>
